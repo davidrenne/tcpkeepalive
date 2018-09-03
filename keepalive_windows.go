062ffba735e2f1516bd2fa45877942a417ec41cf
Carlos C <uldericofilho@gmail.com>
2018-09-03 09:29:40 -0700

    Additional conditional compilation target for windows - dummy

---
+++ keepalive_windows.go
@@ -0,0 +1,17 @@
+package tcpkeepalive
+
+import (
+	"errors"
+)
+
+func setIdle(fd int, secs int) error {
+	return errors.New("not supported")
+}
+
+func setCount(fd int, n int) error {
+	return errors.New("not supported")
+}
+
+func setInterval(fd int, secs int) error {
+	return errors.New("not supported")
+}
